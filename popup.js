bd33c22f9e0d59aec90d3b974e4feec1bac8a3bd
Dhanush Dinesh <dhanushndinesh@gmail.com>
2023-01-04 14:28:44 -0500

    added the add event listener method

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+import {getActiveTabURL} from './utils.js';
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
@@ -9,4 +10,22 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo){
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            //viewBookmarks
+            viewBookmarks(currentVideoBookmarks);
+        });
+    }else{
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title"> This is not a youtube video page.</div>'
+    }
+});
